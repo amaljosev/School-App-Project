657a85bfdd0c3fea06c9379dd31ab2f27f84c663
amaljose <amaljvattakkunnel@gmail.com>
2023-12-12 18:41:31 +0530

    problems are fixed

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,17 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:schoolapp/repositories/core/colors.dart';
-import 'package:schoolapp/screens/admin/admin_screen.dart';
 import 'package:schoolapp/screens/admin/bloc/admin_bloc.dart';
 import 'package:schoolapp/screens/requests/bloc/admin_request_bloc.dart';
 import 'package:schoolapp/screens/student/bloc/student_bloc.dart';
-import 'package:schoolapp/screens/student/student_screen.dart';
 import 'package:schoolapp/screens/teacher/controllers/teacherBloc1/teacher_bloc.dart';
 import 'package:schoolapp/screens/teacher/controllers/teacherBloc2/teacher_second_bloc.dart';
-import 'package:schoolapp/screens/teacher/profile/student_profile.dart';
-import 'package:schoolapp/screens/teacher/teacher_screen.dart';
 import 'package:schoolapp/screens/welcome/bloc/welcome_bloc.dart';
-import 'package:schoolapp/screens/welcome/first_screen.dart';
 import 'package:schoolapp/screens/welcome/splash_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(); 
   runApp(const MyApp());
 }
 
